a4f27bbdf1d9ebc3d1c6ea2aa1908c92bb16ede3
aryankshl <lit2021037@iiitl.ac.in>
2024-10-11 13:55:17 +0530

    Added report generating feature and styled it

--- test.py
+++
@@ -1,5 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-print("Loading model...")
-model = SentenceTransformer('all-mpnet-base-v2')  # The model you're using
-print("Model loaded successfully!")
